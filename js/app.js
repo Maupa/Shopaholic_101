c15be3cf1c80fb5bff923f57cac9381efb0b0fba
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-01-29 07:26:10 +1300

    Added model

--- js/app.js
+++ js/app.js
@@ -4,9 +4,19 @@
 		var options = $.extend({
 			data: '/data/items.json' //This line adding another parameter to the options... ```expands``` it
 		}, params);
+		var models = {
+			item: function(item) {
+				return '<div class="col-4">' +
+						'<div class="item">' +
+							'<img src="http://placehold.it/150x150">' +
+							'<p>' + item.name + '</p>' +
+						'</div>' +
+					'</div>';
+			}
+		}
 		$.getJSON( 'data/items.json', function( data ) {
 			$.each( data.items, function( key, value ) {
-				console.log(value)
+				this.append(models.item(value));
 			});
 		});
 	};
