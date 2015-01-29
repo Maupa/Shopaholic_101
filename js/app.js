f6fbd10de568e3a6c4a1e710b709bd13903842c8
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-01-29 20:33:35 +1300

    Updated model. Added Super for accesing super this object.

--- js/app.js
+++ js/app.js
@@ -4,19 +4,22 @@
 		var options = $.extend({
 			data: '/data/items.json' //This line adding another parameter to the options... ```expands``` it
 		}, params);
+		var Super = this;
 		var models = {
 			item: function(item) {
-				return '<div class="col-4">' +
-						'<div class="item">' +
-							'<img src="http://placehold.it/150x150">' +
-							'<p>' + item.name + '</p>' +
-						'</div>' +
+				return '<div class="col-3">' + 
+						'<div class="item">' + 
+							'<div class="col-12">' + 
+								'<img src="http://placehold.it/150x150" style="width:100%;">' + 
+							'</div>' + 
+							'<p class="name">' + item.name + '<a class="button" data-id="' + item.id + '" href="#">Add to cart</a></p>' + 
+						'</div>' + 
 					'</div>';
 			}
 		}
 		$.getJSON( 'data/items.json', function( data ) {
 			$.each( data.items, function( key, value ) {
-				this.append(models.item(value));
+				$(Super).append(models.item(value));
 			});
 		});
 	};
