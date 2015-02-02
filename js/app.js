5d7a211cd6de46c859cfe94de89a3368344f3698
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-02-02 20:00:51 +1300

    Added localstorage manipulation methods.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@
 		}, params);
 		var Super = this;
 		var jsonData = {};
+		var items = {};
 		var models = {
 			item: function(item) {
 				return '<div class="col-3">' + 
@@ -25,7 +26,7 @@
 					$(Super).append(models.item(value));
 				});
 				$('a.button').click(function(){ //Adds click handler
-					console.log(this);
+					render.cart.add($(this).data('id'));
 				})
 
 			},
@@ -36,16 +37,46 @@
 				
 				$('head').append('<title>' + data.name + '</title>'); //Add name
 			},
-			cart: function(){
+			cart: {
 
+				list: function(){ //Returns array of items.
+					return JSON.parse(localStorage.get('cart'));
+				},
+
+				add: function(id){ //Added id.
+					var temp = JSON.parse(localStorage.getItem('cart'));
+					if(!Array.isArray(temp)) temp = [];
+					temp.push(id.toString());
+					localStorage.setItem('cart', JSON.stringify(temp));
+				},
+
+				remove: function(id){ //Removes id.
+					var temp = JSON.parse(localStorage['cart']);
+					var index = temp.indexOf(id.toString());
+					if(index > -1){
+						temp = temp.splice(index, 1);
+						localStorage.setItem('cart', JSON.stringify(temp));
+					}
+				},
+
+				clear: function(){ //Clears shopping cart.
+					localStorage.setItem('cart', []);
+				}
 			}
 		}
 
 		$.getJSON( 'data/items.json', function( data ) {
+			$.each( data.items, function( key, value ) {
+				items[value.id] = value //Reversing list to object
+			});
+
 			jsonData = data; //Saving data
 			render.desc(data); //Rendering description
 			render.items(data); //Rendering items
-		});
+		})
+		.fail(function(error){
+			console.log(error)
+		})
 	};
 	
 }(jQuery));
